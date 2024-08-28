a1c3fade3a3f9032e0ec9c24ae585d0294485bb7
princedigital01 <145067724+princedigital01@users.noreply.github.com>
2024-08-27 17:25:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(cors({
+  origin: 'https://davis-edoh.onrender.com', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 
